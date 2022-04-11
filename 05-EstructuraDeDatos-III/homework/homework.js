85e86a1255ee6dbb713b29f21711c59e5eb8455c
Damita-Mindful <bds.mindful@gmail.com>
2022-04-11 18:39:17 -0300

    Repositorio M1

---
+++ 05-EstructuraDeDatos-III/homework/homework.js
@@ -0,0 +1,46 @@
+'use strict'
+// resolve estos ejercicios usando recursión
+
+// Binary Seach Tree
+// 'tiene metodos llamados `insert`, `contains`, `depthFirstForEach`, 'breadthFirstForEach' y 'size'
+// corre depth-first (en recorrido "in-order") cuando depthFirstForEach() es ejecutado sin ninguna opcion o con la opcion "in-order
+// corre depth-first (en recorrido "pre-order") cuando depthFirstForEach() es ejecutado con la opcion "pre-order"
+// corre depth-first (en recorrido "post-order" cuando depthFirstForEach() es ejecutado con la opcion "post-order"
+// corre breadth-first cuando breadthFirstForEach() es ejecutado
+// Observar imagen de la carpeta "homework" llamada "bst.png". Allí encontraran dibujado el arbol utilizado para los tests
+
+function BinarySearchTree(value) { 
+  
+}
+
+
+BinarySearchTree.prototype.insert = function (node) { 
+ 
+}
+
+BinarySearchTree.prototype.size = function () {
+
+  
+}
+
+
+BinarySearchTree.prototype.contains = function (value) { 
+  
+} 
+
+BinarySearchTree.prototype.depthFirstForEach = function (cb, order) {
+
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function (cb, array) { // con este metodo vemos todos los 
+         // elementos hermanos antes de bajar al siguiente nivel
+ 
+}
+
+
+// No modifiquen nada debajo de esta linea
+// --------------------------------
+
+module.exports = {
+  BinarySearchTree
+};
